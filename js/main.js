17383d28c4e9110615aca2b3233271858c87ba76
Katerina2020 <kashleva2009@yandex.ru>
2020-03-18 14:09:20 +0300

    add js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,49 @@
 $(document).ready(function(){
     $('.slider').slick({
         infinite: true,
-        slidesToShow: 3,
+        centerPadding: '70px',
+        slidesToShow: 4,
         slidesToScroll: 1, 
         prevArrow:
             '<img class="slick-prev slider__btn_prev slider__btn" src=img/btn.png>',
         nextArrow: 
             '<img class="slick-next slider__btn_next slider__btn" src=img/btn.png>',
     });
+
+    /* Modal */
+
     $('.modal-open').on('click', function(event) {
         event.preventDefault();
         $('.modal').fadeIn();
         $('body').css({ "overflow" : "hidden"});
     });
 
+    /* Close modal */
+
     $('.modal__btn_close').on('click', function(event) {
         event.preventDefault();
         $('.modal').fadeOut();
         $('body').css({ "overflow" : "visible"});
+    });
+
+    /* Close modal by ESC */
+
+    $(document).keydown(function(e) {
+        if (e.keyCode === 27) {
+            e.stopPropagation();
+            $('.modal').fadeOut();
+            $('body').css({ "overflow" : "visible"});
+        }
+    });
+
+    /* Click on the background, but not on the window */
+
+    $('.modal').on('click', function(e) {
+        e.preventDefault();
+        if ($(e.target).closest('.modal__wrap').length == 0) {
+            $(this).fadeOut();		
+            $('body').css({ "overflow" : "visible"});			
+        }
     });
   });
 
