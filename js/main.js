c013840f3410f9a38f2fd4991499ac7fb3b553c0
Katerina2020 <kashleva2009@yandex.ru>
2020-03-18 11:45:00 +0300

    add js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-// $(document).ready(function(){
-//     $('.slider').slick({
-//         slidesToShow: 3,
-//         slidesToScroll: 3
-//     });
-//   });
+$(document).ready(function(){
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1, 
+        prevArrow:
+            '<img class="slick-prev slider__btn_prev slider__btn" src=../img/btn.png>',
+        nextArrow: 
+            '<img class="slick-next slider__btn_next slider__btn" src=../img/btn.png>',
+    });
+    $('.modal-open').on('click', function(event) {
+        event.preventDefault();
+        $('.modal').fadeIn();
+        $('body').css({ "overflow" : "hidden"});
+    });
+
+    $('.modal__btn_close').on('click', function(event) {
+        event.preventDefault();
+        $(this).parents('.modal').fadeOut();
+        $('body').css({ "overflow" : "visible"});
+    });
+  });
+
