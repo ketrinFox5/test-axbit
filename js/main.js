05c45e57eece14cd39f65eac863897f3b74ea3b7
Katerina2020 <kashleva2009@yandex.ru>
2020-03-18 13:34:29 +0300

    modification modal

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
         slidesToShow: 3,
         slidesToScroll: 1, 
         prevArrow:
-            '<img class="slick-prev slider__btn_prev slider__btn" src=../img/btn.png>',
+            '<img class="slick-prev slider__btn_prev slider__btn" src=img/btn.png>',
         nextArrow: 
-            '<img class="slick-next slider__btn_next slider__btn" src=../img/btn.png>',
+            '<img class="slick-next slider__btn_next slider__btn" src=img/btn.png>',
     });
     $('.modal-open').on('click', function(event) {
         event.preventDefault();
@@ -16,7 +16,7 @@ $(document).ready(function(){
 
     $('.modal__btn_close').on('click', function(event) {
         event.preventDefault();
-        $(this).parents('.modal').fadeOut();
+        $('.modal').fadeOut();
         $('body').css({ "overflow" : "visible"});
     });
   });
